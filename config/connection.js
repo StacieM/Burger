53ce6c35288219e6048d96674f8f49ec705004d5
Stacie Meierhoefer <staciemead@gmail.com>
2017-07-08 11:20:15 -0700

    connection string

--- config/connection.js
+++ config/connection.js
@@ -2,17 +2,17 @@ var mysql = require("mysql");
 var connection;
 
 // add JAWSDB for heroku
-if (process.env.JAWSDB_URL) {
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: "127.0.0.1",
-    port: 8889,
-    user: "root",
-    password: "root",
-    database: "burgers_db"
-  });
-}
+// if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection("mysql://ken6aforizal9umh:xc9ohcjftjp721f9@jj820qt5lpu6krut.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/b2efhhawskb86v8k");
+// } else {
+//   connection = mysql.createConnection({
+//     host: "127.0.0.1",
+//     port: 8889,
+//     user: "root",
+//     password: "root",
+//     database: "burgers_db"
+//   });
+// }
 
 // connection = mysql.createConnection({
 //     host: "127.0.0.1",
